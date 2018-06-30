2666d989423d40aa8b19b3025b50504c46aa5481
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-07-01 00:34:34 +0500

    main menu screen created

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { StackNavigator } from 'react-navigation'
+import MainMenuScreen from '../Containers/MainMenuScreen'
 import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
@@ -6,6 +7,7 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  MainMenuScreen: { screen: MainMenuScreen },
   LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen },
 }, {
