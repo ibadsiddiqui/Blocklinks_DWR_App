44d51fa7ebb24c28b57874949b83c5d476c3c895
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-07-01 21:31:33 +0500

    styling added to Menu screen

--- App/Containers/Styles/MainMenuScreenStyle.js
+++ App/Containers/Styles/MainMenuScreenStyle.js
@@ -1,6 +1,32 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { Metrics, ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  container:{
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    paddingTop: 20,
+    backgroundColor: '#006064'
+  },
+  buttonContainer:{ 
+    padding:30,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  button:{
+    fontSize: 20,
+    color: 'white'
+  },
+  titleContainer:{
+    padding: 10,
+    paddingLeft: Metrics.screenWidth * 0.25,
+    marginTop: Metrics.screenHeight * 0.1  
+  },
+  title:{
+    color: 'white',
+    fontSize: 35
+  },
 })
