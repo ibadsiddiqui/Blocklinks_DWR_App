16b503e6e0f08cb6e95d73696af56b49b8d292a8
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-07-01 21:41:17 +0500

    RecieveProduct screen created

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,4 +1,5 @@
 import { StackNavigator } from 'react-navigation'
+import RecieveProductScreen from '../Containers/RecieveProductScreen'
 import MainMenuScreen from '../Containers/MainMenuScreen'
 import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
@@ -7,6 +8,7 @@ import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  RecieveProductScreen: { screen: RecieveProductScreen },
   MainMenuScreen: { screen: MainMenuScreen },
   LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen },
