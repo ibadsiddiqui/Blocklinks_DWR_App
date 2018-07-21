18e892a2c8884bb024bc91819a20632fec2f7f71
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-07-22 02:51:45 +0500

    sale screen updated

--- App/Containers/SaleScreen.js
+++ App/Containers/SaleScreen.js
@@ -1,32 +1,79 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
-import { connect } from 'react-redux'
-// Add Actions - replace 'Your' with whatever your reducer is called :)
-// import YourActions from '../Redux/YourRedux'
+import { 
+  ScrollView,
+  Image, 
+  Text, 
+  KeyboardAvoidingView,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native'
+import { Images } from '../Themes'
 
 // Styles
 import styles from './Styles/SaleScreenStyle'
 
-class SaleScreen extends Component {
+export default class SaleScreen extends Component {
+  onBackBtnPress(){
+    const {navigate} = this.props.navigation;
+    navigate('MainMenuScreen')
+  }
+
   render () {
     return (
-      <ScrollView style={styles.container}>
+      <View style={styles.container}>
         <KeyboardAvoidingView behavior='position'>
-          <Text>SaleScreen</Text>
+
+          <TouchableOpacity onPress={this.onBackBtnPress.bind(this)}>
+            <Image source={Images.backButton} style={styles.backLogo}/>
+          </TouchableOpacity>
+          <View style={[styles.titleContainer, styles.centered]}>
+            <Text style={styles.title}>
+              Sale
+            </Text>
+            <View style={{ borderBottomColor: 'black', borderBottomWidth: 1, width: 100}} />      
+
+          </View>
+
+          <View style={styles.centered}>
+
+            <View style={styles.rowView}>
+              <Text style={styles.rowText}>Unique ID* : </Text>
+              <TextInput style={[styles.field, styles.UniqueIDPadding]}/>
+            </View>
+
+            <View style={styles.rowView}>
+              <Text style={styles.rowText}>Customer Id: </Text>
+              <TextInput style={[styles.field, styles.boxInputPadding]}/>
+            </View>
+
+            <View style={styles.rowView}>
+              <Text style={styles.rowText}>Retailer ID:</Text>
+              <TextInput style={[styles.field, styles.retailerIDPadding]}/>
+            </View>
+
+            <View style={styles.rowView}>
+              <Text style={styles.rowText}>Time & Date:</Text>
+              <TextInput style={[styles.field, styles.TimePadding]}/>
+            </View>
+
+            <View style={styles.rowView}>
+              <Text style={styles.rowText}>Geolocation:</Text>
+              <TextInput style={[styles.field, styles.GeolocationPadding]}/>
+            </View>
+
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity>
+                  <Text style={styles.button}>
+                    Submit
+                  </Text>
+                </TouchableOpacity>
+              </View>
+
+          </View>
+  
         </KeyboardAvoidingView>
-      </ScrollView>
+      </View>
     )
   }
 }
-
-const mapStateToProps = (state) => {
-  return {
-  }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SaleScreen)
